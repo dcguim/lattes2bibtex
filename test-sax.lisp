8f7a7b13770fb1d573fe3630c7d8b61eca5cdeef
Daniel Guimaraes <dcguim@gmail.com>
2015-04-21 20:01:47 -0300

    Two utility functions, to insert a pair to the hash and another one to print the hash/ Modified the sax:start-element to map all the relevant attributes from the lattes xml

--- test-sax.lisp
+++ test-sax.lisp
@@ -1,18 +1,37 @@
 (defparameter *hash* (make-hash-table))
-(defparameter current-article "")
+(defparameter *curr-article* "")
+(defparameter *attr* (list (list 'state "NATUREZA") (list 'title "TITULO-DO-ARTIGO") (list 'year "ANO-DO-ARTIGO") 
+			 (list 'country "PAIS-DE-PUBLICACAO") (list 'language "IDIOMA") (list  'media "MEIO-DE-DIVULGACAO")
+			 (list 'homepage "HOME-PAGE-DO-TRABALHO") (list 'en-title "TITULO-DO-ARTIGO-INGLES")))
+
 
 (defclass bibtex-handler (sax:default-handler)())
-	     
+
+(defun print-hash ()
+  (maphash #'(lambda (k v) 
+	       (progn
+		 (format t "CHAVE:~5t~a~%" (gethash 'key v))
+		 (dolist (at *attr*)
+		   (format t "~a:~25t~a~%" (cadr at) (gethash (car at) v)))
+		 (format t "~%"))) *hash*))
+
+ (defun insert-pair (key value)
+	   (setf (gethash key (gethash *curr-article* *hash*)) value))	     
 
 (defmethod sax:start-element ((h bibtex-handler) (namespace t) (local-name t) (qname t) (attributes t))
   (cond ((equal local-name "ARTIGO-PUBLICADO")
 	 (let ((seq (sax:attribute-value (sax:find-attribute "SEQUENCIA-PRODUCAO" attributes))))
 	   (setf (gethash seq *hash*) (make-hash-table))
-	   (setf current-article seq)
-	   (setf (gethash 'key  (gethash current-article *hash*)) (concatenate 'string "article-" seq))))
+	   (setf *curr-article* seq)
+	   (insert-pair 'key (concatenate 'string "article-" seq))))
 	((equal local-name "DADOS-BASICOS-DO-ARTIGO")
-	(let ((an (sax:attribute-value (sax:find-attribute "TITULO-DO-ARTIGO" attributes))))
-	  (setf (gethash 'title (gethash current-article *hash*)) an)))))
+	 (dolist (elem *attr*)
+	   (let ((e (sax:find-attribute (cadr elem)  attributes)))
+	     (if e
+	         (insert-pair (car elem) (sax:attribute-value e))))))))
+
+	
+	
 
 ;;(defmethod sax:characters ((handler tostring) data)
 ;;         (write-string data (acc handler)))
@@ -24,6 +43,6 @@
 ;; use cases
 ;;; Parse the xml 
 ;;;;  (cxml:parse "<x>111<y>222</y>333</x>" (make-instance 'tostring))
-;;;;  (cxml:parse #p"/Users/dcguim/common-lisp/bibtex-project/hermann-p1.xml" (make-instance 'bibtex-handler))
+;;;;  (cxml:parse #p"/Users/dcguim/common-lisp/xml2bibtex/hermann-p1.xml" (make-instance 'bibtex-handler))
 ;;; Print the hash
-;;;; (maphash #'(lambda (k v) (format t "~a: key:~a, title:~a ~%" k (gethash 'key v) (gethash 'title v))) *hash*)
+;;;(maphash #'(lambda (k v) (format t "~a: key:~a, title:~a ~% year:~a country:~a lang:~a ~% media:~a homepage:~a en-title:~a ~%" k (gethash 'key v) (gethash 'title v) (gethash 'year v) (gethash 'country v) (gethash 'language v) (gethash 'media v) (gethash 'homepage v) (gethash 'en-title v))) *hash*)
