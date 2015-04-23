ac3c131f37adee72d3e9a6f879e3a8b65cc9c528
Daniel Guimaraes <dcguim@gmail.com>
2015-04-23 19:06:08 -0300

    Cleaned up the unnecessary global variables which are now slots of the lattes-handler class

--- test-sax.lisp
+++ test-sax.lisp
@@ -1,48 +1,49 @@
-(defparameter *hash* (make-hash-table))
-(defparameter *curr-article* "")
 (defparameter *attr* (list (list 'state "NATUREZA") (list 'title "TITULO-DO-ARTIGO") (list 'year "ANO-DO-ARTIGO") 
 			 (list 'country "PAIS-DE-PUBLICACAO") (list 'language "IDIOMA") (list  'media "MEIO-DE-DIVULGACAO")
 			 (list 'homepage "HOME-PAGE-DO-TRABALHO") (list 'en-title "TITULO-DO-ARTIGO-INGLES")))
 
 
-(defclass bibtex-handler (sax:default-handler)())
+(defclass lattes-handler (sax:default-handler)
+  ((hash 
+    :initform (make-hash-table)
+    :accessor lh-hash)
+   (current-article
+    :initform ""
+    :accessor lh-curr-art)))
+    
 
-(defun print-hash ()
+(defun print-hash (hash)
   (maphash #'(lambda (k v) 
 	       (progn
 		 (format t "CHAVE:~5t~a~%" (gethash 'key v))
 		 (dolist (at *attr*)
 		   (format t "~a:~25t~a~%" (cadr at) (gethash (car at) v)))
-		 (format t "~%"))) *hash*))
+		 (format t "~%"))) hash))
 
- (defun insert-pair (key value)
-	   (setf (gethash key (gethash *curr-article* *hash*)) value))	     
+ (defun insert-pair (key value obj)
+	   (setf (gethash key (gethash (lh-curr-art obj) (lh-hash obj))) value))
 
-(defmethod sax:start-element ((h bibtex-handler) (namespace t) (local-name t) (qname t) (attributes t))
+(defmethod sax:start-element ((lh lattes-handler) (namespace t) (local-name t) (qname t) (attributes t))
   (cond ((equal local-name "ARTIGO-PUBLICADO")
 	 (let ((seq (sax:attribute-value (sax:find-attribute "SEQUENCIA-PRODUCAO" attributes))))
-	   (setf (gethash seq *hash*) (make-hash-table))
-	   (setf *curr-article* seq)
-	   (insert-pair 'key (concatenate 'string "article-" seq))))
+	   (setf (gethash seq (lh-hash lh)) (make-hash-table))
+	   (setf (lh-curr-art lh) seq)
+	   (insert-pair 'key (concatenate 'string "article-" seq) lh)))
 	((equal local-name "DADOS-BASICOS-DO-ARTIGO")
-	 (dolist (elem *attr*)
-	   (let ((e (sax:find-attribute (cadr elem)  attributes)))
+	 (dolist (at *attr*)
+	   (let ((e (sax:find-attribute (cadr at)  attributes)))
 	     (if e
-	         (insert-pair (car elem) (sax:attribute-value e))))))))
-
+		 (insert-pair (car at) (sax:attribute-value e) lh)))))))
 	
 	
 
-;;(defmethod sax:characters ((handler tostring) data)
-;;         (write-string data (acc handler)))
 
+;; use case
+;;; Make an instance of lattes-handler
+;;; > (defparameter i1 (make-instance 'lattes-handler))
 
-;;(defmethod sax:end-document ((handler tostring))     
-;;  (get-output-stream-string (acc handler)))
-
-;; use cases
 ;;; Parse the xml 
-;;;;  (cxml:parse "<x>111<y>222</y>333</x>" (make-instance 'tostring))
-;;;;  (cxml:parse #p"/Users/dcguim/common-lisp/xml2bibtex/hermann-p1.xml" (make-instance 'bibtex-handler))
+;;; > (cxml:parse #p"/path/to/the/file.xml" i1) 
+
 ;;; Print the hash
-;;;(maphash #'(lambda (k v) (format t "~a: key:~a, title:~a ~% year:~a country:~a lang:~a ~% media:~a homepage:~a en-title:~a ~%" k (gethash 'key v) (gethash 'title v) (gethash 'year v) (gethash 'country v) (gethash 'language v) (gethash 'media v) (gethash 'homepage v) (gethash 'en-title v))) *hash*)
+;;; > (print-hash (lh-hash i1))
